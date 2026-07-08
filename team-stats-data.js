46ca0d137f998f4bc92cd844cfccf4ce436e3716
Northamm101 <mike.northam@gmail.com>
2026-07-07 22:29:57 -0500

    Add initial team stats data structure

---
+++ team-stats-data.js
@@ -0,0 +1,29 @@
+const teamStats = {
+  players: [
+    {
+      name: "Jason",
+      gamesPlayed: 0,
+      rosterOrder: 1
+    },
+    {
+      name: "Jeff",
+      gamesPlayed: 0,
+      rosterOrder: 2
+    },
+    {
+      name: "Tom",
+      gamesPlayed: 0,
+      rosterOrder: 3
+    },
+    {
+      name: "Mike",
+      gamesPlayed: 0,
+      rosterOrder: 4
+    },
+    {
+      name: "Dallas",
+      gamesPlayed: 0,
+      rosterOrder: 5
+    }
+  ]
+};
