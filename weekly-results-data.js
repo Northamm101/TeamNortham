ac4c6dfc4b1cbd4ca3d88d83b056954b4e1dfdab
Northamm101 <mike.northam@gmail.com>
2026-07-09 15:48:34 -0500

    Update game results and team data

--- weekly-results-data.js
+++ weekly-results-data.js
@@ -6,68 +6,52 @@ const weeklyResults = [
     phase: "regular",
 
     games: [
-      {
-        draw: "early",
-        time: "7:00 PM",
-        sheet: 1,
-        teamA: 1,
-        teamB: 2,
-        winner: 1
-      },
-      {
-        draw: "early",
-        time: "7:00 PM",
-        sheet: 2,
-        teamA: 4,
-        teamB: 5,
-        winner: 5
-      },
-      {
-        draw: "early",
-        time: "7:00 PM",
-        sheet: 3,
-        teamA: 8,
-        teamB: 9,
-        winner: 8
-      },
-      {
-        draw: "late",
-        time: "9:15 PM",
-        sheet: 1,
-        teamA: 7,
-        teamB: 3,
-        winner: 7
-      },
-      {
-        draw: "late",
-        time: "9:15 PM",
-        sheet: 2,
-        teamA: 6,
-        teamB: 10,
-        winner: 10
-      },
-      {
-        draw: "late",
-        time: "9:15 PM",
-        sheet: 3,
-        teamA: 11,
-        teamB: 12,
-        winner: 11
-      }
-    ],
-
-    teamNortham: {
-      opponent: 3,
-      result: "W",
-      teamScore: 8,
-      opponentScore: 5,
-      ends: 7,
-      draw: "late",
-      time: "9:15 PM",
-      sheet: 1,
-      rockColor: "Red",
-      lineup: ["Jason", "Jeff", "Tom", "Mike"],
-      notes: "Temporary master-results test"
-    }
+  {
+    draw: "early",
+    time: "7:00 PM",
+    sheet: 1,
+    teamA: 12,
+    teamB: 1,
+    winner: 1
+  },
+  {
+    draw: "early",
+    time: "7:00 PM",
+    sheet: 2,
+    teamA: 9,
+    teamB: 11,
+    winner: 11
+  },
+  {
+    draw: "early",
+    time: "7:00 PM",
+    sheet: 3,
+    teamA: 2,
+    teamB: 8,
+    winner: 8
+  },
+  {
+    draw: "late",
+    time: "9:15 PM",
+    sheet: 1,
+    teamA: 7,
+    teamB: 3,
+    winner: 7
+  },
+  {
+    draw: "late",
+    time: "9:15 PM",
+    sheet: 2,
+    teamA: 10,
+    teamB: 5,
+    winner: 10
+  },
+  {
+    draw: "late",
+    time: "9:15 PM",
+    sheet: 3,
+    teamA: 4,
+    teamB: 6,
+    winner: 6
   }
-];
+],
