07a5d7b42a2e049be4a716781957347a7451e876
Northamm101 <mike.northam@gmail.com>
2026-07-10 12:04:46 -0500

    Replace team names with generic identifiers
    Updated team names to generic placeholders.

--- league-schedule-data.js
+++ league-schedule-data.js
@@ -291,18 +291,18 @@ const leagueSchedule = [
 ];
 
 const teamNames = {
-  1: "Team Fouasse",
-  2: "Team Audette",
-  3: "Team M. Rondeau",
-  4: "Team Messner",
-  5: "Team Brunette",
-  6: "Team G. Rondeau",
-  7: "Team Northam",
-  8: "Team Bjarnason",
-  9: "Team Lyle",
-  10: "Team Ridge",
-  11: "Team Stevenson",
-  12: "Team Hallonquist"
+  1: "Team 1",
+  2: "Team 2",
+  3: "Team 3",
+  4: "Team 4",
+  5: "Team 5",
+  6: "Team 6",
+  7: "Team 7",
+  8: "Team 8",
+  9: "Team 9",
+  10: "Team 10",
+  11: "Team 11",
+  12: "Team 12"
 };
 
 const teamNorthamNumber = 7;
