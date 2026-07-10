975a09a0638a096d6576edb273ded3662832316a
Northamm101 <mike.northam@gmail.com>
2026-07-10 14:48:12 -0500

    Refine comments in player availability data
    Updated comments to clarify the usage of the player availability data structure.

--- player-availability-data.js
+++ player-availability-data.js
@@ -1,21 +1,40 @@
 const playerAvailability = [
   /*
-    Add one object whenever a player is unavailable.
+    Add one object only when there is at least one
+    availability issue for a game.
 
     Example:
 
     {
       date: "2026-10-01",
       displayDate: "October 1",
-      player: "Mike",
-      status: "Unavailable"
+
+      unavailable: [
+        "Mike"
+      ],
+
+      availableIfNeeded: [
+        "Dallas"
+      ],
+
+      lastResort: [
+        "Jeff"
+      ]
     }
 
-    Status values:
+    Status meanings:
 
-    "Unavailable"
-    "Sitting but Available"
-    "Last Resort"
+    unavailable
+      Player cannot play.
+
+    availableIfNeeded
+      Player is available if the team is short players.
+
+    lastResort
+      Player should only be contacted if absolutely necessary.
+
+    If everyone is available for a game,
+    do not add an object.
 
     Delete the object once the game has been played.
   */
