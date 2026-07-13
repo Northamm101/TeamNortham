c4aafa82ffeb08912b2c9b4d3c652ad19292ddfb
Northamm101 <mike.northam@gmail.com>
2026-07-13 10:22:46 -0500

    Update player-availability-data.js

--- player-availability-data.js
+++ player-availability-data.js
@@ -4,19 +4,83 @@ const playerAvailability = [
     displayDate: "October 1",
 
     unavailable: [
-      "Mike"
-    ],
-
-    availableIfNeeded: [
       "Dallas"
     ],
 
-    lastResort: [
+    availableIfNeeded: [],
+
+    lastResort: [],
+
+    spares: []
+  },
+
+  {
+    date: "2026-10-08",
+    displayDate: "October 8",
+
+    unavailable: [],
+
+    availableIfNeeded: [
+      "Jason"
+    ],
+
+    lastResort: [],
+
+    spares: []
+  },
+
+  {
+    date: "2026-10-15",
+    displayDate: "October 15",
+
+    unavailable: [
+      "Tom",
       "Jeff"
     ],
 
+    availableIfNeeded: [],
+
+    lastResort: [],
+
     spares: [
       "Richard"
+    ]
+  },
+
+  {
+    date: "2026-10-22",
+    displayDate: "October 22",
+
+    unavailable: [
+      "Tom"
+    ],
+
+    availableIfNeeded: [],
+
+    lastResort: [
+      "Mike"
+    ],
+
+    spares: []
+  },
+
+  {
+    date: "2026-10-29",
+    displayDate: "October 29",
+
+    unavailable: [
+      "Dallas",
+      "Jason",
+      "Jeff"
+    ],
+
+    availableIfNeeded: [],
+
+    lastResort: [],
+
+    spares: [
+      "Richard",
+      "Liam"
     ]
   }
 ];
