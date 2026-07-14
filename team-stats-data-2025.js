66861f44815dfb516d1cd3a63a2900bb5e0077cb
Northamm101 <mike.northam@gmail.com>
2026-07-14 11:17:05 -0500

    Update team-stats-data-2025.js

--- team-stats-data-2025.js
+++ team-stats-data-2025.js
@@ -31,5 +31,73 @@ const teamStats2025 = {
     }
   ],
 
-  games: []
-};
+  games: [
+  {
+    date: "2025-10-02",
+    displayDate: "October 2, 2025",
+    phase: "Regular Season",
+    draw: "late",
+    sheet: 2,
+    opponent: 6,
+    result: "W",
+    teamScore: 7,
+    opponentScore: 4,
+    lineup: [
+      "Jason",
+      "Jeff",
+      "Tom",
+      "Mike"
+    ]
+  },
+  {
+    date: "2025-10-09",
+    displayDate: "October 9, 2025",
+    phase: "Regular Season",
+    draw: "early",
+    sheet: 3,
+    opponent: 11,
+    result: "W",
+    teamScore: 14,
+    opponentScore: 1,
+    lineup: [
+      "Cory",
+      "Dallas",
+      "Jeff",
+      "Mike"
+    ]
+  },
+  {
+    date: "2025-10-16",
+    displayDate: "October 16, 2025",
+    phase: "Regular Season",
+    draw: "late",
+    sheet: 3,
+    opponent: 7,
+    result: "L",
+    teamScore: 1,
+    opponentScore: 7,
+    lineup: [
+      "Jason",
+      "Jeff",
+      "Tom",
+      "Mike"
+    ]
+  },
+  {
+    date: "2025-10-23",
+    displayDate: "October 23, 2025",
+    phase: "Regular Season",
+    draw: "early",
+    sheet: 2,
+    opponent: 2,
+    result: "L",
+    teamScore: 4,
+    opponentScore: 9,
+    lineup: [
+      "Cory",
+      "Dallas",
+      "Tom",
+      "Mike"
+    ]
+  }
+]
