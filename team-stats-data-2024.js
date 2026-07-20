68e6d84287d4994dd3b0e0c46851e0529a220721
Northamm101 <mike.northam@gmail.com>
2026-07-20 15:56:57 -0500

    Add team stats for the 2024–25 season

--- team-stats-data-2024.js
+++ team-stats-data-2024.js
@@ -1,1 +1,16 @@
+const teamStats2024 = {
+  season: "2024–25",
+  teamNumber: 2,
+  trackRockColors: false,
 
+  scoring: {
+    win: 10,
+    loss: 5,
+    tie: 7,
+    defaultLoss: 0
+  },
+
+  roster: [],
+
+  games: []
+};
