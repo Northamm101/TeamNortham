fae15fa8ac686f4ec3af462e51c88d3e5f1dbdd6
Northamm101 <mike.northam@gmail.com>
2026-07-21 12:10:01 -0500

    Rename teamStats2024 to teamStats2023

---
+++ team-stats-data-2023.js
@@ -0,0 +1,293 @@
+const teamStats2023 = {
+  season: "2023–24",
+  teamNumber: 2,
+  trackRockColors: false,
+  trackScores: false,
+  trackLineups: false,
+  roster: [],
+
+  scoring: {
+    win: 10,
+    loss: 5,
+    tie: 7,
+    defaultLoss: 0
+  },
+
+  games: [
+    {
+      date: "2024-10-03",
+      displayDate: "October 3, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 2,
+      opponent: 9,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-10-10",
+      displayDate: "October 10, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 3,
+      opponent: 11,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-10-17",
+      displayDate: "October 17, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 2,
+      opponent: 8,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-10-24",
+      displayDate: "October 24, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 3,
+      opponent: 7,
+      result: "T",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-10-31",
+      displayDate: "October 31, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 2,
+      opponent: 6,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-11-07",
+      displayDate: "November 7, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 1,
+      opponent: 1,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-11-14",
+      displayDate: "November 14, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 3,
+      opponent: 12,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-11-21",
+      displayDate: "November 21, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 1,
+      opponent: 3,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-11-28",
+      displayDate: "November 28, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 3,
+      opponent: 10,
+      result: "T",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-12-05",
+      displayDate: "December 5, 2024",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 3,
+      opponent: 5,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-12-12",
+      displayDate: "December 12, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 1,
+      opponent: 4,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2024-12-19",
+      displayDate: "December 19, 2024",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 3,
+      opponent: 5,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-01-02",
+      displayDate: "January 2, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 1,
+      opponent: 10,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-01-09",
+      displayDate: "January 9, 2025",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 2,
+      opponent: 1,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-01-16",
+      displayDate: "January 16, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 2,
+      opponent: 6,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-01-23",
+      displayDate: "January 23, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 1,
+      opponent: 12,
+      result: "L",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-01-30",
+      displayDate: "January 30, 2025",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 3,
+      opponent: 7,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-02-06",
+      displayDate: "February 6, 2025",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 2,
+      opponent: 3,
+      result: "W",
+      phase: "Regular Season",
+      lineup: []
+    },
+
+    {
+      date: "2025-02-13",
+      displayDate: "February 13, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 1,
+      opponent: 5,
+      result: "L",
+      phase: "Playoffs",
+      lineup: []
+    },
+
+    {
+      date: "2025-02-20",
+      displayDate: "February 20, 2025",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 1,
+      opponent: 7,
+      result: "W",
+      phase: "Playoffs",
+      lineup: []
+    },
+
+    {
+      date: "2025-02-27",
+      displayDate: "February 27, 2025",
+      time: "9:15 PM",
+      draw: "late",
+      sheet: 2,
+      opponent: 1,
+      result: "T",
+      phase: "Playoffs",
+      lineup: []
+    },
+
+    {
+      date: "2025-03-06",
+      displayDate: "March 6, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 3,
+      opponent: 3,
+      result: "L",
+      phase: "Playoffs",
+      lineup: []
+    },
+
+    {
+      date: "2025-03-13",
+      displayDate: "March 13, 2025",
+      time: "7:00 PM",
+      draw: "early",
+      sheet: 2,
+      opponent: 12,
+      result: "W",
+      phase: "Playoffs",
+      lineup: []
+    }
+  ]
+};
